fd4b0e5f3d0266b09cc296f696eebfdc568544d6
lijason103 <lijason103@gmail.com>
2018-09-29 22:08:20 -0700

    Changed background subtractor Added gray shadow filter Added object hitbox Added trails

--- object_tracker.py
+++ object_tracker.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
@@ -7,7 +6,6 @@ import cv2
 import imutils
 import time
  
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
 	help="path to the (optional) video file")
@@ -15,39 +13,26 @@ ap.add_argument("-b", "--buffer", type=int, default=64,
 	help="max buffer size")
 args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
 # greenLower = (21, 88, 127)
 # greenUpper = (167, 177, 255)
 greenLower = (0, 0, 0)
-greenUpper = (255, 255, 255)
+greenUpper = (20, 20, 20)
 # greenLower = (10, 1, 151)
 # greenUpper = (41, 66, 199)
 pts = deque(maxlen=args["buffer"])
  
-# if a video path was not supplied, grab the reference
-# to the webcam
 if not args.get("video", False):
 	vs = VideoStream(src=0).start()
- 
-# otherwise, grab a reference to the video file
 else:
 	vs = cv2.VideoCapture(args["video"])
- 
-# allow the camera or video file to warm up
+
 time.sleep(2.0)
-fgbg = cv2.createBackgroundSubtractorMOG2()
-# keep looping
+# fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+# fgbg = cv2.createBackgroundSubtractorMOG2(500, 16, True)
+fgbg = cv2.createBackgroundSubtractorKNN(500, 400, True)
 while True:
-	# grab the current frame
 	frame = vs.read()
- 
-	# handle the frame from VideoCapture or VideoStream
 	frame = frame[1] if args.get("video", False) else frame
- 
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
 	if frame is None:
 		break
  
@@ -55,32 +40,45 @@ while True:
 	frame = imutils.resize(frame, width=600)
 	# blur the frame to reduce high frequency noise
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	# convert to HSV
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
- 
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
+	# apply background subtractor
 	mask = fgbg.apply(blurred)
-	# mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+	# filter gray shadow
+	mask = cv2.inRange(mask, 200, 255)
+	# mask = cv2.erode(mask, None, iterations=2)
+	# mask = cv2.dilate(mask, None, iterations=2)
 
-    # show the frame to our screen
-	cv2.imshow("Frame", mask)
+	# contour
+	contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	contours = contours[0] if imutils.is_cv2() else contours[1]
+	center = None
+	if len(contours) > 0:
+		c = max(contours, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		if radius > 10 and radius < 20:
+			if M["m00"] > 0:
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			center = (int(x), int(y))
+			cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+	pts.appendleft(center)
+
+	# Draw trails
+	# for i in range(1, len(pts)):
+	# 	if pts[i - 1] is None or pts[i] is None:
+	# 		continue
+	# 	thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+	# 	cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
-    # if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
 
-# if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
 	vs.stop()
- 
-# otherwise, release the camera
 else:
 	vs.release()
  
-# close all windows
 cv2.destroyAllWindows()
